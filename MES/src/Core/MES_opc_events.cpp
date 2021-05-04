4a0c6ea9142325f7b48c2cf5613c40ea7dc7963a
up201707219 <up201707219@fe.up.pt>
2021-05-04 16:40:23 +0100

    slight change in reqorder func

--- MES/src/Core/MES_opc_events.cpp
+++ MES/src/Core/MES_opc_events.cpp
@@ -2,12 +2,12 @@
 
 void MES::onSendTransform()
 {
-    UA_Variant order_C1_piece_init;
-    UA_Variant_init(&order_C1_piece_init);
+    // UA_Variant order_C1_piece_init;
+    // UA_Variant_init(&order_C1_piece_init);
     uint16_t piece_init = 1;
-    UA_Variant_setScalar(&order_C1_piece_init, &piece_init, &UA_TYPES[UA_TYPES_UINT16]);
+   // UA_Variant_setScalar(&order_C1_piece_init, &piece_init, &UA_TYPES[UA_TYPES_UINT16]);
     std::string node = std::string(OPC_GLOBAL_NODE_STR) + std::string("orders_C1[1].init_p");
-    fct_client.writeValue(UA_NODEID_STRING_ALLOC(4, node.c_str()), order_C1_piece_init);
+    fct_client.writeValue(UA_NODEID_STRING_ALLOC(4, node.c_str()), piece_init);
 
     UA_Variant order_C1_quantity;
     UA_Variant_init(&order_C1_quantity);
@@ -15,13 +15,6 @@ void MES::onSendTransform()
     UA_Variant_setScalar(&order_C1_quantity, &quantity, &UA_TYPES[UA_TYPES_INT16]);
     node = std::string(OPC_GLOBAL_NODE_STR) + std::string("orders_C1[1].n_pieces");
     fct_client.writeValue(UA_NODEID_STRING_ALLOC(4, node.c_str()), order_C1_quantity);
-
-    UA_Variant order_C1_clear_flag;
-    UA_Variant_init(&order_C1_clear_flag);
-    bool req_orderC1_flag = false;
-    UA_Variant_setScalar(&order_C1_clear_flag, &req_orderC1_flag, &UA_TYPES[UA_TYPES_BOOLEAN]);
-    node = std::string(OPC_GLOBAL_NODE_STR) + std::string("req_orderC1_flag");
-    fct_client.writeValue(UA_NODEID_STRING_ALLOC(4, node.c_str()), order_C1_clear_flag);
 
     //fct_client;
 
