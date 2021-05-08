ec849c25969d238e51e7980ccf538cdb40cd3904
up201707219 <up201707219@fe.up.pt>
2021-05-08 15:01:35 +0100

    Rota calculada (erro: na escrita do tool_time)

--- MES/src/Core/MES_opc_events.cpp
+++ MES/src/Core/MES_opc_events.cpp
@@ -3,19 +3,19 @@
 #define TOOLSET_BUFLEN 4
 
 // Auxiliar Declarations
-void chooseTools(std::vector<int> &tools, uint16_t& piece_intermediate, std::vector<int> &tool_time, const TransformOrder &next_order);
-void chooseToolSet(int *tool_set, const std::vector<int> &tools);
-void chooseRoute(std::vector<int> &route, const int *tool_set, const std::vector<int> &tools);
+void chooseTools(std::vector<int16_t> &tools, uint16_t& piece_intermediate, std::vector<uint64_t> &tool_time, const TransformOrder &next_order);
+void chooseToolSet(int16_t *tool_set, const std::vector<int16_t> &tools);
+void chooseRoute(std::vector<int16_t> &route, const int16_t *tool_set, const std::vector<int16_t> &tools);
 
 struct opc_order
 {
     uint16_t init_p;
-    int quantity;
-    int to_do;
-    int done;
-    int *tool_set;
-    std::vector<int> path;
-    std::vector<int> tool_time;
+    int16_t quantity;
+    int16_t to_do;
+    int16_t done;
+    int16_t *tool_set;
+    std::vector<int16_t> path;
+    std::vector<uint64_t> tool_time;
     bool warehouse_intermediate;
     uint16_t piece_intermediate;
 };
@@ -26,7 +26,7 @@ void MES::onSendTransform(int cell)
     ss_node << "orders_C" << cell;
     opc_order order;
     // TransformOrder* next_order = scheduler.getTransformOrders()[0];
-    auto next_order = std::make_unique<TransformOrder>(111, 0, 3, P1, P8, 1, 30);
+    auto next_order = std::make_unique<TransformOrder>(111, 0, 3, P1, P5, 1, 30);
     if(next_order == nullptr){
         return;
     }
@@ -36,21 +36,21 @@ void MES::onSendTransform(int cell)
     order.done = 0;
 
     // Get tools
-    std::vector<int> tools; tools.reserve(6);
+    std::vector<int16_t> tools; tools.reserve(6);
     uint16_t piece_intermediate;
     chooseTools(tools, piece_intermediate, order.tool_time, *next_order);
     order.piece_intermediate = piece_intermediate;
     //MES_TRACE("Piece Intermediate: {}", order.piece_intermediate);
-    //MES_TRACE("Tool time: {}; {}; {}; {}; {}; {}; {};", order.tool_time[0], order.tool_time[1], order.tool_time[2], order.tool_time[3], order.tool_time[4], order.tool_time[5],  order.tool_time[6]);
+    MES_TRACE("Tool time: {}; {}; {}; {}; {}; {}; {};", order.tool_time[0], order.tool_time[1], order.tool_time[2], order.tool_time[3], order.tool_time[4], order.tool_time[5],  order.tool_time[6]);
 
     // Choose toolset
-    int tool_set[4] = {0,0,0,0};
+    int16_t tool_set[4] = {0,0,0,0};
     chooseToolSet(tool_set, tools);
     order.tool_set = tool_set;
     MES_TRACE("Tool_set: {}; {}; {}; {};", order.tool_set[0], order.tool_set[1], order.tool_set[2], order.tool_set[3]);
 
     // Choose route
-    std::vector<int> path; path.reserve(6);
+    std::vector<int16_t> path; path.reserve(8);
     chooseRoute(path, tool_set, tools);
     order.path = path;
     //MES_TRACE("Path: {}; {}; {}; {}; {}; {}; {}; {};", order.path[0], order.path[1], order.path[2], order.path[3], order.path[4], order.path[5], order.path[6], order.path[7]);
@@ -62,54 +62,36 @@ void MES::onSendTransform(int cell)
             order.warehouse_intermediate = true;
         }
     }
-    //MES_TRACE("Warehouse intermediate: {}", order.warehouse_intermediate);
-
-    // Transform vectors into arrays
-    int array_path[order.path.size()];
-
-    for(int i = 0; i<order.path.size(); i++){
-        array_path[i] = order.path[i];
-    }
-
-    int array_tool_time[order.tool_time.size()];
-
-    for(int i = 0; i<order.path.size(); i++){
-        array_tool_time[i] = order.tool_time[i];
-    }
+    // MES_TRACE("Warehouse intermediate: {}", order.warehouse_intermediate);
 
     // Write to factory
 
-    std::string node = std::string(OPC_GLOBAL_NODE_STR) + std::string(ss_node.str()) + std::string("[1].init_p");
+    std::string node = std::move(std::string(OPC_GLOBAL_NODE_STR) + std::string(ss_node.str()) + std::string("[1].init_p"));
     fct_client.writeValue(UA_NODEID_STRING_ALLOC(4, node.c_str()), order.init_p);
 
-    node = std::string(OPC_GLOBAL_NODE_STR) + std::string(ss_node.str()) + std::string("[1].quantity");
-    fct_client.writeValue(UA_NODEID_STRING_ALLOC(4, node.c_str()), order.quantity);
+    node = std::move(std::string(OPC_GLOBAL_NODE_STR) + std::string(ss_node.str()) + std::string("[1].quantity"));
+    int debug = fct_client.writeValue(UA_NODEID_STRING_ALLOC(4, node.c_str()), order.quantity);
 
-    node = std::string(OPC_GLOBAL_NODE_STR) + std::string(ss_node.str()) + std::string("[1].to_do");
+    node = std::move(std::string(OPC_GLOBAL_NODE_STR) + std::string(ss_node.str()) + std::string("[1].to_do"));
+    fct_client.writeValue(UA_NODEID_STRING_ALLOC(4, node.c_str()), order.to_do);
+
+    node = std::move(std::string(OPC_GLOBAL_NODE_STR) + std::string(ss_node.str()) + std::string("[1].done"));
     fct_client.writeValue(UA_NODEID_STRING_ALLOC(4, node.c_str()), order.done);
 
-    node = std::string(OPC_GLOBAL_NODE_STR) + std::string(ss_node.str()) + std::string("[1].tool_set");
+    node = std::move(std::string(OPC_GLOBAL_NODE_STR) + std::string(ss_node.str()) + std::string("[1].tool_set"));
     fct_client.writeValue(UA_NODEID_STRING_ALLOC(4, node.c_str()), order.tool_set, TOOLSET_BUFLEN);
 
-    node = std::string(OPC_GLOBAL_NODE_STR) + std::string(ss_node.str()) + std::string("[1].path");
-    fct_client.writeValue(UA_NODEID_STRING_ALLOC(4, node.c_str()), array_path, order.path.size());
+    node = std::move(std::string(OPC_GLOBAL_NODE_STR) + std::string(ss_node.str()) + std::string("[1].path"));
+    fct_client.writeValue(UA_NODEID_STRING_ALLOC(4, node.c_str()), &order.path[0], 8);
 
-    node = std::string(OPC_GLOBAL_NODE_STR) + std::string(ss_node.str()) + std::string("[1].tool_time");
-    fct_client.writeValue(UA_NODEID_STRING_ALLOC(4, node.c_str()), array_tool_time, order.tool_time.size());
+    node = std::move(std::string(OPC_GLOBAL_NODE_STR) + std::string(ss_node.str()) + std::string("[1].tool_time"));
+    MES_TRACE(fct_client.writeValue(UA_NODEID_STRING_ALLOC(4, node.c_str()), &order.tool_time[0], 8));
 
-    node = std::string(OPC_GLOBAL_NODE_STR) + std::string(ss_node.str()) + std::string("[1].warehouse_intermediate");
+    node = std::move(std::string(OPC_GLOBAL_NODE_STR) + std::string(ss_node.str()) + std::string("[1].warehouse_intermidiate"));
     fct_client.writeValue(UA_NODEID_STRING_ALLOC(4, node.c_str()), order.warehouse_intermediate);
 
-    node = std::string(OPC_GLOBAL_NODE_STR) + std::string(ss_node.str()) + std::string("[1].piece_intermediate");
+    node = std::move(std::string(OPC_GLOBAL_NODE_STR) + std::string(ss_node.str()) + std::string("[1].piece_intermidiate"));
     fct_client.writeValue(UA_NODEID_STRING_ALLOC(4, node.c_str()), order.piece_intermediate);
-
-    /*orders_C1[1].init_p;
-    orders_C1[1].warehouse_intermediate;
-    orders_C1[1].quantity;
-    orders_C1[1].to_do;
-    orders_C1[1].tool_set[4];
-    orders_C1[1].path[8];
-    orders_C1[1].tool_time[8];*/
 }
 
 void MES::onSendUnload()
@@ -146,7 +128,7 @@ void MES::onFinishProcessing()
 // #################################################################################################################
 
 // get tools
-void chooseTools(std::vector<int>& tools, uint16_t& piece_intermediate, std::vector<int> &tool_time, const TransformOrder& next_order)
+void chooseTools(std::vector<int16_t>& tools, uint16_t& piece_intermediate, std::vector<uint64_t> &tool_time, const TransformOrder& next_order)
 {
     piece_t piece_act = next_order.getInitial();
     int i = 0;
@@ -211,7 +193,7 @@ void chooseTools(std::vector<int>& tools, uint16_t& piece_intermediate, std::vec
 }
 
 // choose toolset
-void chooseToolSet(int *tool_set, const std::vector<int> &tools)
+void chooseToolSet(int16_t *tool_set, const std::vector<int16_t> &tools)
 {
     for(int i = 0; i<tools.size(); i++){
         if(i <= 3){
@@ -230,7 +212,7 @@ void chooseToolSet(int *tool_set, const std::vector<int> &tools)
 }
 
 // choose route
-void chooseRoute(std::vector<int> &route, const int *tool_set, const std::vector<int> &tools)
+void chooseRoute(std::vector<int16_t> &route, const int16_t *tool_set, const std::vector<int16_t> &tools)
 {
     int mac_act = 0; //starts at warehouse
     for(int i = 0; i<tools.size(); i++){
